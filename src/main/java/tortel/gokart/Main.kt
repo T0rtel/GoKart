f005b179ded3f476a32d22df231afd3c063c33cb
Ido <hilloy15@gmail.com>
2024-07-15 19:20:58 +0300

    comment

--- src/main/java/tortel/gokart/Main.kt
+++ src/main/java/tortel/gokart/Main.kt
@@ -34,7 +34,7 @@ class Main : JavaPlugin() {
         dataFolderDir = dataFolder
         instance = this
 
-        loadConfigs()
+        loadConfigs() //loads configs
         registerEvents()
         registerCommands()
         setupVehicleTickSystem()
